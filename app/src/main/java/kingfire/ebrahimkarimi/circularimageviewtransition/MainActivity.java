a97e347fec3917f96f5ab421d75b65e5b7bc552d
Ebrahim Karimi <ebrahimkarimi78@yahoo.com>
2018-06-27 22:06:05 +0430

    init

---
+++ app/src/main/java/kingfire/ebrahimkarimi/circularimageviewtransition/MainActivity.java
@@ -0,0 +1,13 @@
+package kingfire.ebrahimkarimi.circularimageviewtransition;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
